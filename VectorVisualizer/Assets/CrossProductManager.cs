411ab4c27c63fe137889c2c6fcd7797fcec56b37
mcroswell <mike@coloradogamecoders.com>
2019-03-11 00:18:08 -0600

    Added CrossProduct visualization.  Minor visual improvements.
    Added CrossProduct visualization.
    Smoothed the lowPolyCylinder to make it look more like a rope.
    Added some materials and
    a new scene, under Scenes called DotAndCross.unity
    Made materials use a larger tile value for the y direction to make surface look more like rope.

---
+++ VectorVisualizer/Assets/CrossProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CrossProductManager : MonoBehaviour
+{
+
+    public LineAsCylinder _A_LineCylinder;
+    public LineAsCylinder _B_LineCylinder;
+    public LineAsCylinder _CrossProduct;
+    public bool _reverseOrder = false;
+    private Vector3 a;
+    private Vector3 b;
+    private Vector3 c;
+
+    [ExecuteInEditMode]
+    void CalculateCrossProd()
+    {
+        a = _A_LineCylinder._end;
+        b = _B_LineCylinder._end;
+        if (_reverseOrder)
+            c = Vector3.Cross(b, a);
+        else
+            c = Vector3.Cross(a, b);
+
+    }
+
+    [ExecuteInEditMode]
+    void Update()
+    {
+        CalculateCrossProd();
+        _CrossProduct._end = c;
+
+    }
+}
